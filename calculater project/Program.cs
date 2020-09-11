658215bdec444aaabe2f4627526b9fb57aa1ff81
saber zaidi <sabir_xaidi@yahoo.com>
2020-09-11 23:46:00 +0200

    Add project files.

---
+++ calculater project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace calculater_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my calculater");
+            int a, b, c;
+                Console.WriteLine("Please Enter First no");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Second no");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("YourAnswer =" +c);
+            Console.ReadLine();
+        }
+    }
+}
